1c33872a9272e55c778be972965b227eec0a656d
yashwanth0098 <yashwanth0098@gmail.com>
2025-08-17 17:28:47 +0530

    Logging and Exception add

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    
+    requirements_lst:list[str] = []
+    try:
+        with open('requirements.txt') as file_obj:
+            requirements = file_obj.readlines()
+            for line in requirements:
+                requirements=line.strip()
+                if requirements and requirements != '-e .':
+                    requirements_lst.append(requirements)
+    except FileNotFoundError:
+        print('requirements.txt not found')
+    return requirements_lst
+
+
+setup(
+    name= "networksecurity",
+    version= "0.0.1",
+    author= "Yashwanth K",
+    author_email="yahswanth0098@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements()
+    
+)
